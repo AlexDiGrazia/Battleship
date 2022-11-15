3c86f608908d04cfcb63b35375f1eae6c53a8bce
Alex Digrazia <alexndigrazia@hotmail.com>
2022-11-15 02:20:31 -0800

    completed ship placement function

--- temporary.js
+++ temporary.js
@@ -1,11 +1,8 @@
-console.clear();
-
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const final = [];
 const ship5 = [];
 const shipFive = [];
 const coords = [];
-
 
 // Make grid
 
@@ -20,11 +17,10 @@ function makeGrid (number) {
 }
 const grid = makeGrid(10);
 
-
 // Ship Class 
 
 class Ship {
-  constructor (name, length, array, direction, xCoord, yCoord) {
+  constructor(name, length, array, direction, xCoord, yCoord) {
     this.name = name;
     this.length = length;
     this.array = array;
@@ -35,29 +31,6 @@ class Ship {
 
   shipDirection(element) {
     element.direction = Math.floor(Math.random() * 2);
-  } 
-
-  path(ship) {
-    if (ship.direction === 0) {
-      ship.horizontalShip(ship);
-      ship.noOverlap(ship);
-      ship.splice(ship);
-    } else if (ship.direction === 1) {
-      ship.verticalShip(ship);
-      ship.noOverlap(ship);
-      ship.splice(ship);
-    }
-  }  
-
-  noOverlap(ship) {
-    if (!ship.array.includes(null) && ship != []) {
-      return ship;
-    } else {
-      ship.length = 0;
-      ship.randomNumber(ship);
-      ship.path(ship);
-      // noOverlap(ship);
-    }
   }
 
   randomNumber(shipCoord) {
@@ -65,50 +38,62 @@ class Ship {
     shipCoord.yCoord = Math.floor(Math.random() * 10);
   }
 
-  horizontalShip(ship) {
-    for(i = 0; i < ship.length; i++) {
-      if (ship.xCoord > ship.length) {
-        const selectShipCoordinates = () => grid[ship.yCoord][ship.xCoord - i];
-        const shipCoordinate = selectShipCoordinates();
-        ship.array.push(shipCoordinate);
-      } else {
-        const selectShip = () => grid[ship.yCoord][ship.xCoord + i];
-        const someShipCoord = selectShip();
-        ship.array.push(someShipCoord);
+  makeShip(ship) {
+    ship.array = [];
+    if (ship.direction === 0) {
+      for (i = 0; i < ship.length; i++) {
+        if (ship.xCoord > ship.length) {
+          const selectShipCoordinates = () =>
+            grid[ship.yCoord][ship.xCoord - i];
+          const shipCoordinate = selectShipCoordinates();
+          ship.array.push(shipCoordinate);
+        } else {
+          const selectShip = () => grid[ship.yCoord][ship.xCoord + i];
+          const someShipCoord = selectShip();
+          ship.array.push(someShipCoord);
+        }
+      }
+    } else if (ship.direction === 1) {
+      for (i = 0; i < ship.length; i++) {
+        if (ship.yCoord <= ship.length) {
+          const makeShip = () => grid[ship.yCoord + i][ship.xCoord];
+          const newShip = makeShip();
+          ship.array.push(newShip);
+        } else {
+          const makeShip = () => grid[ship.yCoord - i][ship.xCoord];
+          const newShip = makeShip();
+          ship.array.push(newShip);
+        }
       }
     }
   }
-  
-  verticalShip(ship) {
-    for(i = 0; i < ship.length; i++) {
-      if(ship.yCoord <= ship.length) {
-        const makeShip = () => grid[ship.yCoord + i][ship.xCoord];
-        const newShip = makeShip();
-        ship.array.push(newShip);
-      } else {
-        const makeShip = () => grid[ship.yCoord - i][ship.xCoord];
-        const newShip = makeShip();
-        ship.array.push(newShip);
-      }
+
+  noOverlap(ship) {
+    ship.makeShip(ship);
+    if (!ship.array.includes(null) && ship.array != []) {
+      // console.log('yes');
+      return ship;
+    } else {
+      // console.log('TRY AGAIN!!!!!');
+      ship.array = [];
+      ship.randomNumber(ship);
+      ship.noOverlap(ship);
     }
-    return ship.yCoord;
   }
-  
-// no overlap old location
 
   splice(ship) {
     if (ship.direction === 0) {
       for (i = 0; i < ship.length; i++) {
         if (ship.xCoord > ship.length) {
-          grid[ship.yCoord].splice((ship.xCoord - i), 1, null);
+          grid[ship.yCoord].splice(ship.xCoord - i, 1, null);
         } else {
-          grid[ship.yCoord].splice((ship.xCoord + i), 1, null);
+          grid[ship.yCoord].splice(ship.xCoord + i, 1, null);
         }
       }
     } else if (ship.direction === 1) {
       if (ship.yCoord <= ship.length) {
         for (i = ship.yCoord; i < ship.yCoord + ship.length; i++) {
-          grid[(i)].splice(ship.xCoord, 1, null);
+          grid[i].splice(ship.xCoord, 1, null);
         }
       } else if (ship.yCoord > ship.length) {
         for (i = ship.yCoord; i > ship.yCoord - ship.length; i--) {
@@ -117,8 +102,13 @@ class Ship {
       }
     }
   }
-}
 
+  path(ship) {
+    ship.makeShip(ship);
+    ship.noOverlap(ship);
+    ship.splice(ship);
+  }
+}
 
 const carrierShip = new Ship('Carrier', 5, []);
 const cruiser = new Ship('Cruiser', 4, []);
@@ -135,25 +125,4 @@ shipArray.forEach((element) => element.path(element));
 console.clear();
 
 // console.log(shipArray);
-
 console.table(grid);
-
-
-
-
-
-
-
-
-
-
-
-// shipArray.forEach((element) => element.verticalShip(element));
-// shipArray.forEach((element) => element.horizontalShip(element));
-
-
-
-
-
-
-
